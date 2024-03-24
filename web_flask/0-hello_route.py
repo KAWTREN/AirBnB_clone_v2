d3e322d04e3c685d790b312d4ef255e3464fb9dc
kawtar <kawtarennabirha@gmail.com>
2024-03-24 13:07:28 +0000

    p

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-""" module doc """
 from flask import Flask
 
 app = Flask(__name__)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-"""script that starts a Flask web application"""
+
 from flask import Flask
 
 app = Flask(__name__)
