c55a8bfa4318760dd02ef9aed991a8ecaacd2c6c
kawtar <kawtarennabirha@gmail.com>
2024-03-24 11:11:01 +0000

    task1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""Write a script that starts a Flask web application"""
-=======
 """script that starts a Flask web application"""
 from flask import Flask
 
@@ -18,4 +15,3 @@ def hbnb():
     return "HBNB"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
->>>>>>> eedb17c7eb4ffabe5017d9e58adef3dc8b241c3a
