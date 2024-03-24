4091675d67bcd74b8d9d2e331deb7fc49eeb5140
kawtar <kawtarennabirha@gmail.com>
2024-03-24 13:52:42 +0000

    task

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -27,7 +27,7 @@ def python_text(text):
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def is_number(n):
-    return f'{n} is a number'
+    return '{ } is a number'.format(n)
 
 
 if __name__ == '__main__':
