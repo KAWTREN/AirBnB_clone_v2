15310bb4d7287f04e9269efe8ad3eaebe029018f
kawtar <kawtarennabirha@gmail.com>
2024-03-24 20:46:14 +0000

    task

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """def doc"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """def doc"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """def doc"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """def doc"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    """def doc"""
+    return '{ } is a number'.format(n)
+
+@app.route('/number_template/<n>', strict_slashes=False)
+def number_template(n):
+    """def doc"""
+    return render_template('5-number.html', n=n)
+@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+def number_odd_or_even(n):
+   """def doc"""
+   if n % 2 == 0:
+     parity = 'even'
+   else:
+     parity = 'odd'
+   return render_template('6-number_odd_or_even.html', n=n, parity=parity)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
